1ae5ffd1791503e54948208eb6cb6e0b3a4dd785
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2019-03-25 22:14:05 +0200

    Adds webpack merge setup & styled components basic implementation

---
+++ webpack.config.prod.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpackBase = require("./webpack.config.base");
+
+module.exports = merge.smart(webpackBase, {
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
+});
